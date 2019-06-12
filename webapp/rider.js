5da55e05e7835b918dd985cb56d0c1e5ad56f264
Dave Briccetti <daveb@davebsoft.com>
2019-06-12 12:30:21 -0700

    Improve rider code

--- webapp/rider.js
+++ webapp/rider.js
@@ -1,3 +1,4 @@
+/** Manages an elevator rider */
 export default class Rider {
     constructor(p, startFloor, destFloor, cars, stats) {
         this.p = p;
@@ -5,17 +6,17 @@ export default class Rider {
         this.destFloor = destFloor;
         this.cars = cars;
         this.stats = stats;
+
         this.createStates();
         this.state = this.STATE_ARRIVING;
-        this.carDims = cars[0].settings.geom.car;
-        this.height = p.randomGaussian(this.carDims.y / 2, 4);
+        this.carGeom = cars[0].settings.geom.car;
+        this.height = p.randomGaussian(this.carGeom.y / 2, 4);
         this.width = Math.max(6, p.randomGaussian(this.height / 4, 4));
-        const neg1IfComingFromRight = this.randomDirection();
-        const enterX = p.width / 2 - neg1IfComingFromRight * p.width / 2;
+        const travelDirection = this.p.random([-1, 1]);
+        const enterX = p.width / 2 - travelDirection * p.width / 2;
         this.pos = p.createVector(enterX, p.yFromFloor(this.startFloor), this.randomFloorZ());
-        const waitX = enterX + neg1IfComingFromRight * p.randomGaussian(p.width / 4, p.width / 10);
-        this.waitPos = p.createVector(waitX, this.pos.y, this.pos.z);
-        this.arrivingPath = [this.waitPos];
+        const waitX = enterX + travelDirection * p.randomGaussian(p.width / 4, p.width / 10);
+        this.arrivingPath = [p.createVector(waitX, this.pos.y, this.pos.z)];
         this.carIn = undefined;
         this.color = [p.random(255), p.random(255), p.random(255)];
         this.movementPerMs = p.randomGaussian(300, 50) / 1000;
@@ -32,12 +33,8 @@ export default class Rider {
         this.STATE_EXITED   = 6;
     }
 
-    randomDirection() {
-        return this.p.random(1) < 0.5 ? -1 : 1;
-    }
-
     randomFloorZ() {
-        return this.p.map(this.p.random(1), 0, 1, -20, 20);
+        return this.p.lerp(-20, 20, this.p.random(1));
     }
 
     update() {
@@ -77,7 +74,7 @@ export default class Rider {
 
     outsideDoorPos(openCar) {
         return this.p.createVector(openCar.carCenterX() + this.fuzz(2),
-            this.pos.y, openCar.settings.geom.carCenterZ + this.carDims.z + this.fuzz(2));
+            this.pos.y, openCar.settings.geom.carCenterZ + this.carGeom.z + this.fuzz(2));
     }
 
     ride() {
@@ -93,18 +90,18 @@ export default class Rider {
     }
 
     setBoardingPath(car) {
-        const cd = this.carDims;
-        const insideCar = this.p.createVector(car.carCenterX() + this.fuzz(cd.x * 0.4), this.pos.y,
-            car.settings.geom.carCenterZ + this.fuzz(cd.z * 0.4));
+        const cg = this.carGeom;
+        const insideCar = this.p.createVector(car.carCenterX() + this.fuzz(cg.x * 0.4), this.pos.y,
+            car.settings.geom.carCenterZ + this.fuzz(cg.z * 0.4));
         this.boardingPath = [this.outsideDoorPos(car), insideCar];
     }
 
     setExitingPath(car) {
         const p = this.p;
         const nearDoorInsideCar = p.createVector(car.carCenterX() + this.fuzz(2), this.pos.y,
-            car.settings.geom.carCenterZ + this.carDims.z / 2 - 5 + this.fuzz(2));
+            car.settings.geom.carCenterZ + this.carGeom.z / 2 - 5 + this.fuzz(2));
         const outsideDoor = this.outsideDoorPos(car);
-        const exitPoint = p.createVector(p.width / 2 - this.randomDirection() * p.width / 2,
+        const exitPoint = p.createVector(p.width / 2 - this.p.random([-1, 1]) * p.width / 2,
             this.pos.y, this.randomFloorZ());
         this.exitingPath = [nearDoorInsideCar, outsideDoor, exitPoint];
     }
@@ -130,7 +127,8 @@ export default class Rider {
         this.millisAtLastMove = now;
         const pointerToDest = p5.Vector.sub(dest, this.pos);
         const distToDest = pointerToDest.mag();
-        const step = p5.Vector.mult(pointerToDest.normalize(), Math.min(distToDest, this.movementPerMs * millisSinceLastStep));
+        const stepMagnitude = Math.min(distToDest, this.movementPerMs * millisSinceLastStep);
+        const step = p5.Vector.mult(pointerToDest.normalize(), stepMagnitude);
         this.pos.add(step);
         return p5.Vector.sub(dest, this.pos).mag();
     }
